067cd9c5ed5c3f5a2c92d454d40d0175553b82d6
finleyhewson <62248168+finleyhewson@users.noreply.github.com>
2020-05-31 17:59:42 +0100

    Update Position.py

--- Position.py
+++ Position.py
@@ -34,7 +34,7 @@ class position:
         return self._pos, self._r, self._conf
 
     def loop(self):
-        while self.running:
+        if self.running:
             self._pos, self._r, self._conf, _ = self.t265.get_frame()
             #self.setNorthOffset( self.pixhawkObj.compass_heading )
             self.current_time_us = int(round(time.time() * 1000000))
@@ -50,7 +50,7 @@ class position:
             time.sleep(0.01)
     
     def conf_loop(self):
-        while self.running:
+        if self.running:
             if self.pixhawkObj.enable_update_tracking_confidence_to_gcs:    
                 self.pixhawkObj.send_tracking_confidence_to_gcs(self._conf)
             time.sleep(0.01)
